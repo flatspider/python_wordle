dd04cb5a5fa280224bdbe607a9263493f83106d8
Conor McManamon <c.s.mcmanamon@gmail.com>
2023-02-21 14:09:36 -0500

    Basic wordle functioning

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from random import randint
 from colorama import Fore, Back, Style
 
 
-print(Style.RESET_ALL)
-
 # Have a list of secret words.
 secret_dictionary = ['adult', 'affix', 'afoot']
 size_of_dictionary = len(secret_dictionary) - 1
@@ -17,6 +15,11 @@ secret_word = secret_dictionary[0]
 size_secret_word = 5
 guess_list = ["_ _ _ _ _"]
 letter_array = []
+color_list = []
+
+# The letter array contains the truth. Are the guesses identifying correct letters?
+# The entire word that was guessed needs to be printed, while secretly checking if it matches
+# letters in the secret word.
 
 
 # Wordle requires that each guess be five letters.
@@ -32,13 +35,6 @@ class Main:
         for each in range(0, size_secret_word):
             letter_array.append("_")
 
-    def print_current_letters(self):
-        for each in letter_array:
-            if each.isalpha():
-                print(Back.GREEN + each, end=" ")
-            else:
-                print(Back.BLACK + each, end=" ")
-
     def modify_letters_displayed(self):
         pass
 
@@ -48,26 +44,29 @@ class Main:
             Main.take_in_guess(self)
         if guess == secret_word and self.number_of_guesses > 0:
             Main.cycle_through_letters(self, guess)
-            Main.print_current_letters(self)
-            print(Back.BLACK + "\nYOU WIN!!")
+            # Main.print_current_letters(self)
+            print(Style.RESET_ALL + "\nYOU WIN!!")
             exit()
         else:
             guess_list.append(guess)
             self.number_of_guesses = self.number_of_guesses - 1
             Main.cycle_through_letters(self, guess)
-            Main.print_current_letters(self)
-            print(Style.RESET_ALL)
-            print(guess_list)
+            # Main.print_current_letters(self)
+            # print(guess_list, end="\n")
 
     def cycle_through_letters(self, guess):
         for each in range(0, 5):
             if guess[each] == secret_word[each]:
                 letter_array[each] = guess[each]
+                print(Back.GREEN + guess[each], end=" ")
+            else:
+                print(Back.BLACK + guess[each], end=" ")
+        print(Style.RESET_ALL)
 
 
 game = Main()
 game.start_game()
-game.print_current_letters()
+# game.print_current_letters()
 while game.number_of_guesses > 0:
     game.take_in_guess()
 
